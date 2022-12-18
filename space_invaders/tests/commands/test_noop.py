500b8652c0fa91cb2d9a5e68792c8cf332071acd
vadim.pochivalin <vadim.pochivalin@x5.ru>
2022-12-18 15:45:53 +0500

    Добавлен простой тест команды

---
+++ space_invaders/tests/commands/test_noop.py
@@ -0,0 +1,11 @@
+from space_invaders.engine.interfaces.command import Command
+
+
+class NoopCommand(Command):
+
+    def execute(self) -> None:
+        ...
+
+
+def test_command_execute():
+    assert NoopCommand().execute() is None
