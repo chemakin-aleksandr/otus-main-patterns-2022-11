c6e43e2c6efe2a41d3de484ab4ae3e3d39ccada6
vadim.pochivalin <vadim.pochivalin@x5.ru>
2023-01-12 23:58:05 +0500

    Базовый обработчик исключения

---
+++ space_invaders/engine/interfaces/__init__.py
@@ -0,0 +1,7 @@
+from space_invaders.engine.interfaces.command import Command
+from space_invaders.engine.interfaces.exception_handler import ExceptionHandler
+
+__all__ = (
+    'Command',
+    'ExceptionHandler',
+)
